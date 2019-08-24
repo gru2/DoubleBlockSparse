08b2e0b61547a40a327512ad6e4a8beb430e5008
Radovan Obradovic <Radovan.Obradovic@gmail.com>
2019-08-24 16:31:32 +0200

    Unit test testEigenUtils_toEigen_and_toMatrix added.

--- src/test_double_block_sparse.cpp
+++ src/test_double_block_sparse.cpp
@@ -2,12 +2,25 @@
 #include <CudaUtils.h>
 #include <Usutf.h>
 #include <CudaUtils.h>
+#include <EigenUtils.h>
+#include <Eigen/Core>
 
 USUTF_TEST(testDeviceAllocate)
 {
 	void *x = CudaUtils::deviceAllocate(1);
 	Usutf::test(x != 0);
 	CudaUtils::deviceFree(x);
+}
+
+USUTF_TEST(testEigenUtils_toEigen_and_toMatrix)
+{
+	Eigen::MatrixXf a_ref(2, 3);
+	a_ref <<
+	1.0f, 2.3f, -1.3f,
+	4.5f, 2.2f, 11.4f;
+	CudaUtils::MatrixF a = EigenUtils::toMatrix(a_ref);
+	Eigen::MatrixXf a_test = EigenUtils::toEigen(a);
+	Usutf::test(a_test == a_ref);
 }
 
 int main(int argc, char *argv[])
