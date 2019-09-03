a7450813050698ee3b983c21eb06e9ab50421e6f
Radovan Obradovic <Radovan.Obradovic@gmail.com>
2019-09-03 14:11:38 +0200

    Unit test testCudaUtils_gemmTiled_3 added.

--- src/test_double_block_sparse.cpp
+++ src/test_double_block_sparse.cpp
@@ -88,6 +88,21 @@ USUTF_TEST(testCudaUtils_gemmTiled_2)
 	Usutf::test(EigenUtils::almostEqual(c_test, c_ref));
 }
 
+USUTF_TEST(testCudaUtils_gemmTiled_3)
+{
+	Eigen::MatrixXf ae(256, 4 * 256), be(4 * 256, 3 * 128);
+	ae.setRandom();
+	be.setRandom();
+	Eigen::MatrixXf c_ref = ae * be;
+	CudaUtils::MatrixF a = CudaUtils::toDevice(EigenUtils::toMatrix(ae));
+	CudaUtils::MatrixF b = CudaUtils::toDevice(EigenUtils::toMatrix(be));
+	CudaUtils::MatrixF c_device = CudaUtils::allocateMatrixOnDeviceF(c_ref.rows(), c_ref.cols());
+	CudaUtils::gemmTiled(c_device, a, b);
+	CudaUtils::MatrixF c_host = CudaUtils::toHost(c_device);
+	Eigen::MatrixXf c_test = EigenUtils::toEigen(c_host);
+	Usutf::test(EigenUtils::almostEqual(c_test, c_ref));
+}
+
 int main(int argc, char *argv[])
 {
 	std::cout << "test lib double_block_sparse...\n";	
